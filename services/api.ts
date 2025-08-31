7054affdc522dd93d3f07a152ca06ffd605b2a82
Hong Than Brathna <hangbrathna10@gmail.com>
2025-08-31 20:14:26 +0700

    Add movie detail

--- services/api.ts
+++ services/api.ts
@@ -28,4 +28,22 @@ export const fetchMovies = async ({ query }: { query: string }) => {
 };
 
 
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+    try {
+         const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`, {
+             method: 'GET',
+             headers: TMDB_CONFIG.headers,
+         });
 
+         if (!response.ok) {
+             throw new Error(`Could not fetch movie data: ${response.status} ${response.statusText}`);
+         }
+
+         const data = await response.json();
+
+         return data;
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+}
